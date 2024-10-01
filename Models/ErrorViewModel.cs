57bb10ac454eaf239108b9fe2e41a8891252b4b4
Admin-CFT <Admin-CFT@DESKTOP-7UVJT27>
2024-09-30 22:56:31 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tia_Paty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
